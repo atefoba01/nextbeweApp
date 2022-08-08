2582bfcedaf2a712e16b0eae07160849fdc92aad
Veekshan A <alladaveekshan@gmail.com>
2022-08-08 19:21:18 +0530

    Add files via upload

---
+++ components/job-details.js
@@ -0,0 +1,122 @@
+const clientTemplate = document.createElement("template");
+clientTemplate.setAttribute("id", "details");
+clientTemplate.innerHTML = `
+    <style>
+    .item {
+        margin: 0 0 30px 0;
+        padding: 0;
+        border-bottom: 1px solid #bbbdbf;
+        padding-bottom: 30px;
+      }
+      
+      .item .title {
+        font-family: "Roboto";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 24px;
+        color: #064daa;
+      }
+      
+      .item .content {
+        font-family: "Roboto";
+        font-style: normal;
+        overflow: hidden;
+        margin-bottom: 10px;
+      }
+      .item button {
+        background: #E6F1FF;
+        border-radius: 4px;
+        padding: 5px 20px;
+        color: #064DAA;
+        border: none;
+        outline: none;
+      }
+    </style>
+    <div class="item">
+        <h1 id="title" class="title"></h1>
+        <div id="content" class="content">  
+            
+        </div>
+        <button id="click">Apply</button>
+    </div>
+`;
+
+class JobDetails extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(clientTemplate.content.cloneNode(true));
+    this.shadowRoot.querySelector("#title").innerText =
+      this.getAttribute("title");
+    this.shadowRoot.querySelector("#content").innerHTML =
+      this.getAttribute("content");
+  }
+
+  connectedCallback() {
+    navigate(this);
+  }
+
+  attributeChangedCallback(prop, oldValue, newValue) {
+    navigate(this);
+  }
+}
+
+customElements.define("job-details", JobDetails);
+
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+function navigate(elem) {
+  const shadow = elem.shadowRoot;
+  const click = shadow?.querySelector("#click");
+  console.log(shadow?.querySelector("#click"));
+  click.addEventListener("click", (_event) => {
+    modal.style.display = "block";
+  });
+}
+
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+window.onload = async (_event) => {
+  document.querySelector("#career-details").innerHTML =
+    '<h1 class="title text-center">Loading...</h1>';
+
+  const requestOptions = {
+    method: "GET",
+  };
+
+  await fetch("http://localhost:3000/api/singlejob?postId=" + getUrlParameter('id'), requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result?.data) {
+        const element = result?.data;
+        let careerContent = "";
+          careerContent += `
+          <job-details
+              title="${element.jobRole}"
+              content="${element.jobDescription}"
+          ></job-details>`;
+        document.querySelector("#career-details").innerHTML = careerContent;
+      } else {
+        document.querySelector(
+          "#career-details"
+        ).innerHTML = `<h1 class="title text-center">No jobs...</h1>`;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      document.querySelector(
+        "#career-details"
+      ).innerHTML = `<h1 class="title text-center">Unable to fetch...</h1>`;
+    });
+};
---
+++ components/job-item.js
@@ -0,0 +1,116 @@
+const clientTemplate = document.createElement("template");
+clientTemplate.setAttribute("id", "job");
+clientTemplate.innerHTML = `
+    <style>
+    .item {
+        margin: 0 0 30px 0;
+        padding: 0;
+        border-bottom: 1px solid #bbbdbf;
+        padding-bottom: 30px;
+      }
+      
+      .item .title {
+        font-family: "Roboto";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 24px;
+        color: #064daa;
+      }
+      
+      .item .content {
+        font-family: "Roboto";
+        font-style: normal;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        margin-bottom: 10px;
+      }
+      .item button {
+        background: #E6F1FF;
+        border-radius: 4px;
+        padding: 5px 20px;
+        color: #064DAA;
+        border: none;
+        outline: none;
+      }
+    </style>
+    <div class="item">
+        <h1 id="title" class="title"></h1>
+        <div id="content" class="content">  
+            
+        </div>
+        <button id="click">View more</button>
+    </div>
+`;
+
+class JobItem extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(clientTemplate.content.cloneNode(true));
+    this.shadowRoot.querySelector("#title").innerText =
+      this.getAttribute("title");
+    this.shadowRoot.querySelector("#content").innerHTML =
+      this.getAttribute("content");
+
+    this.shadowRoot.querySelector("#click").dataset.id =
+      this.getAttribute("id");
+  }
+
+  connectedCallback() {
+    navigate(this);
+  }
+
+  attributeChangedCallback(prop, oldValue, newValue) {
+    navigate(this);
+  }
+}
+
+customElements.define("job-item", JobItem);
+
+function navigate(elem) {
+  const shadow = elem.shadowRoot;
+  const click = shadow?.querySelector("#click");
+  console.log(shadow?.querySelector("#click"));
+  click.addEventListener("click", (_event) => {
+    window.location = "career-details.html?id=" + click.getAttribute("data-id");
+  });
+}
+
+window.onload = async (_event) => {
+  document.querySelector("#career").innerHTML =
+    '<h1 class="title text-center">Loading...</h1>';
+
+  const requestOptions = {
+    method: "GET",
+  };
+
+  await fetch("http://localhost:3000/api/jobpostings", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      if (result?.data) {
+        let careerContent = "";
+        result?.data.forEach((element) => {
+          careerContent += `
+          <job-item 
+              id="${element._id}"
+              title="${element.jobRole}"
+              content="${element.jobDescription}"
+          ></job-item>`;
+        });
+
+        document.querySelector("#career").innerHTML = careerContent;
+      } else {
+        document.querySelector(
+          "#career"
+        ).innerHTML = `<h1 class="title text-center">No jobs...</h1>`;
+      }
+    })
+    .catch((error) => {
+      document.querySelector(
+        "#career"
+      ).innerHTML = `<h1 class="title text-center">Unable to fetch...</h1>`;
+    });
+};
